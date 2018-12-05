2eccb31dbef3e9a73c87cf51363964648918c5b4
Ryan Vinyard <rav.ryanvinyard@gmail.com>
2018-12-05 14:01:27 -0800

    add AcmeDrill

---
+++ AcmeDrill.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to Acme Accounting Systems");
+        Console.WriteLine("Remember, we're \"Accounting\" on you!");
+        Console.ReadLine();
+    }
+}
+
