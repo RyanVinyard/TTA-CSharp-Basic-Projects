aaaf52495496e31b9ee847cdd2cf35abace3a580
Ryan Vinyard <rav.ryanvinyard@gmail.com>
2018-12-26 17:24:38 -0800

    finish ExceptionDrill

--- ExceptionDrill/ExceptionDrill/Program.cs
+++ ExceptionDrill/ExceptionDrill/Program.cs
@@ -4,12 +4,47 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ExceptionDrill
+
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        List<int> numberedList = new List<int>();
+        int[] numbers = { 1, 12, 6, 44, 55 };
+        numberedList.AddRange(numbers);
+
+        
+
+        try
         {
+            Console.WriteLine("Enter a number and I'll divide 1, 12, 6, 44, and 55 by it.");
+            int userNumber = Convert.ToInt16(Console.ReadLine());
+            int result = 0;
+
+            foreach (int number in numberedList)
+            {
+                result = number / userNumber;
+                Console.WriteLine(result);
+            }
         }
+        catch (FormatException)
+        {
+            Console.WriteLine("That's not a number, try again.");
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("You can't divide by zero!");
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("I'm not sure what you did to get here, but you did something wrong.");
+        }
+       
+
+        Console.ReadLine();
+
+
+
     }
 }
+
