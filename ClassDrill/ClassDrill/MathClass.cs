5d8a82c0e721cb0327ed02b7265593f1ed0824f3
Ryan Vinyard <rav.ryanvinyard@gmail.com>
2019-02-22 15:51:44 -0800

    start classdrill

---
+++ ClassDrill/ClassDrill/MathClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDrill
+{
+    class MathClass
+    {
+
+        public int Value { get; set; }
+
+    }
+}
---
+++ ClassDrill/ClassDrill/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathClass mathOne = new MathClass();
+            mathOne.Value = 1;
+
+            Console.WriteLine(mathOne.Value);
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
