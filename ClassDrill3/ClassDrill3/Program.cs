c5a08609ec843c2c91e09526a3c500bc7d40d469
Ryan Vinyard <rav.ryanvinyard@gmail.com>
2019-03-08 20:03:41 -0800

    add ClassDrill3

---
+++ ClassDrill3/ClassDrill3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDrill3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestClass testClass = new TestClass();
+
+            int userInteger = 12;
+            double userDecimal = 0.556;
+            string userString = "54";
+
+
+            userInteger = testClass.TestMethod(userInteger);
+            
+
+            userDecimal = testClass.TestMethod(userDecimal);
+            
+
+            userString = testClass.TestMethod(userString);
+            
+
+            Console.WriteLine("The returned values are: " + userInteger + " , " + userDecimal + " , " + userString);
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ ClassDrill3/ClassDrill3/TestClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDrill3
+{
+    class TestClass
+    {
+        public int TestMethod(int userInteger)
+        {
+            userInteger = userInteger + 10;
+            return userInteger;
+        }
+
+        public double TestMethod(double userDecimal)
+        {
+            userDecimal = userDecimal * 2;
+            return userDecimal;
+        }
+
+        public string TestMethod(string userString)
+        {
+            int temp = Convert.ToInt32(userString);
+            temp = temp / 2;
+            
+            userString = Convert.ToString(temp);
+
+
+
+
+            return userString;            
+        }
+    }
+}
