e6a9ff36efa544d49ccbac5c841ac89902fb588a
Ryan Vinyard <rav.ryanvinyard@gmail.com>
2019-03-08 19:11:08 -0800

    add ClassDrill2

---
+++ ClassDrill2/ClassDrill2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDrill2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestClass testClass = new TestClass();
+            int parameter1 = 10;
+            int parameter2 = 20;
+            testClass.VoidMethod(parameter1, parameter2);
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ ClassDrill2/ClassDrill2/TestClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDrill2
+{
+    class TestClass
+    {
+        public void VoidMethod(int parameter1, int parameter2)
+        {
+            int number = parameter1 + 30;
+            Console.WriteLine("The second parameter is: " + parameter2 + ".");
+
+
+        }
+    }
+}
