666a2e7019fb82c3a429545feab227d7b77d0e7f
Ryan Vinyard <rav.ryanvinyard@gmail.com>
2019-03-11 14:55:21 -0700

    start InheritanceDrill

---
+++ InheritanceDrill/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.FirstName = "Ryan";
+            person.LastName = "Vinyard";
+
+            person.SayName();
+
+            Console.ReadLine();
+
+        }
+    }
+}
