ffc9edb6dc21b3450a18204d52e109afa8d1c7c1
Ryan Vinyard <rav.ryanvinyard@gmail.com>
2019-03-11 16:09:58 -0700

    add InterfaceDrill, copy from previous drill

---
+++ InterfaceDrill/InterfaceDrill/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassDrill
+{
+    public abstract class Person
+    {
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+
+        public abstract void SayName();
+
+    }
+}
