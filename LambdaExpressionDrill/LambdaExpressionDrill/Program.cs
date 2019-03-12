37204c6433ce5cd777dbf2598405c2267aca5d99
Ryan Vinyard <rav.ryanvinyard@gmail.com>
2019-03-12 12:26:11 -0700

    begin LambdaExpressionDrill

---
+++ LambdaExpressionDrill/LambdaExpressionDrill/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaExpressionDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
