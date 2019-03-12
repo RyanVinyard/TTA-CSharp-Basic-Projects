50aa3267e3e397100eb3fc47d4a157d7fcc6b027
Ryan Vinyard <rav.ryanvinyard@gmail.com>
2019-03-12 16:30:44 -0700

    add DateTimeDrill

---
+++ DateTimeDrill/DateTimeDrill/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime currentTime = DateTime.Now;
+            Console.WriteLine("The current time is: " + currentTime);
+            Console.WriteLine("Give me a number of hours to add to the current time: ");
+            int extraHours = Convert.ToInt32(Console.ReadLine());
+            
+            DateTime newCurrentTime = currentTime.AddHours(extraHours);
+            Console.WriteLine("Adding in " + extraHours + " hours, we have a new time of: " + newCurrentTime);
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
