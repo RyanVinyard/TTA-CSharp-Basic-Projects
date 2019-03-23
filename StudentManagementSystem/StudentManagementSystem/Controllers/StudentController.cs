c8216bc73b1d56217bc5161fcf537daa00d586e6
Ryan Vinyard <rav.ryanvinyard@gmail.com>
2019-03-22 18:06:07 -0700

    create ability to add students

--- StudentManagementSystem/StudentManagementSystem/Controllers/StudentController.cs
+++ StudentManagementSystem/StudentManagementSystem/Controllers/StudentController.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using StudentManagementSystem.Models;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,12 +11,60 @@ namespace StudentManagementSystem.Controllers
 {
     public class StudentController : Controller
     {
-        private string _connectionString = @"Data Source=VILGARDEMACHINE\SQLEXPRESS;Initial Catalog=School;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"
+        private string _connectionString = @"Data Source=VILGARDEMACHINE\SQLEXPRESS;Initial Catalog=School;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
 
         // GET: Student
         public ActionResult Index()
         {
+            string queryString = "SELECT * FROM STUDENTS";
+            List<Student> students = new List<Student>();
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Student student = new Student();
+                    student.Id = Convert.ToInt32(reader["Id"]);
+                    student.FirstName = reader["FirstName"].ToString();
+                    student.LastName = reader["LastName"].ToString();
+                    students.Add(student);
+                }
+                connection.Close();
+            }
+
+                return View(students);
+        }
+
+        public ActionResult Add()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(Student student)
+        {
+            string queryString = @"INSERT INTO STUDENTS (FirstName, LastName) VALUES (@FirstName, @LastName)";
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                command.Parameters.Add("@FirstName", SqlDbType.VarChar);
+                command.Parameters.Add("@LastName", SqlDbType.VarChar);
+
+                command.Parameters["@FirstName"].Value = student.FirstName;
+                command.Parameters["@LastName"].Value = student.LastName;
+
+                connection.Open();
+                command.ExecuteNonQuery();
+
+                connection.Close();
+
+            }
+            return RedirectToAction("Index");
         }
     }
 }
