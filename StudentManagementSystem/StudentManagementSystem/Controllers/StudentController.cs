2a4bf9b0ab930bfa3a0e78263b8275802ad37c70
Ryan Vinyard <rav.ryanvinyard@gmail.com>
2019-03-22 17:28:54 -0700

    add StudentManagementSystem

---
+++ StudentManagementSystem/StudentManagementSystem/Controllers/StudentController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudentManagementSystem.Controllers
+{
+    public class StudentController : Controller
+    {
+        private string _connectionString = @"Data Source=VILGARDEMACHINE\SQLEXPRESS;Initial Catalog=School;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"
+
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
